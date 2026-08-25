5778576fc0f2b30639c4fd7da58bb01d73da1795
Vegard Totland <vegardtot@uia.no>
2026-08-25 10:22:48 +0200

    add submission step 1_1

--- Submissions/Submission-01/main.cpp
+++ Submissions/Submission-01/main.cpp
@@ -1,7 +1,39 @@
-
 #include <iostream>
 
+#include "template.h"
+
+/// Disable automatic padding for the purpose to see
+/// that the sizeof TMyStruct is the sum of the fields
+#pragma pack(push, 1)
+struct TMyStruct {
+    double foo;
+    int bar;
+};
+#pragma pack(pop)
+
+struct TStudent {
+    unsigned int grade;
+    bool operator>(const TStudent &other) const { return grade > other.grade; }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    constexpr int a = 3;
+    constexpr double b = 3.1;
+
+    PrintTypeSize(a);
+    PrintTypeSize(b);
+    PrintTypeSize(TMyStruct{
+        .foo = b,
+        .bar = a,
+    });
+
+    std::cout << "Max(1, 5) = " << GetMax(1, 5) << std::endl;
+
+    constexpr TStudent s1 = {.grade = 3};
+    constexpr TStudent s2 = {.grade = 5};
+
+    std::cout << "Max(s1, s2) = " << GetMax(s1, s2).grade << std::endl;
+
     return 0;
 }
--- Submissions/Submission-01/template.h
+++ Submissions/Submission-01/template.h
@@ -3,5 +3,13 @@
 #ifndef _TEMPLATE_H
 #define _TEMPLATE_H
 
+#include <iostream>
+
+template <typename T> void PrintTypeSize(T v) {
+    std::cout << "Size: " << sizeof(T) << std::endl;
+}
+
+template <typename T> T GetMax(T a, T b) { return a > b ? a : b; }
+
 
 #endif //_TEMPLATE_H
