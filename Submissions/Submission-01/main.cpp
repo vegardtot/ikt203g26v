3833f0ecba1a4e1449eedf6f9aabf29c9f462729
Vegard Totland <vegardtot@uia.no>
2026-08-25 22:25:09 +0200

    add submission step 1_2 + 2_a+b

--- Submissions/Submission-01/main.cpp
+++ Submissions/Submission-01/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <string>
 
 #include "template.h"
 
-/// Disable automatic padding for the purpose to see
-/// that the sizeof TMyStruct is the sum of the fields
+////////////////////////////////////////////////////////////////////////////////////
+/// Step 1
+////////////////////////////////////////////////////////////////////////////////////
+
+
+// Disable automatic padding for the purpose to see
+// that the sizeof TMyStruct is the sum of the fields
 #pragma pack(push, 1)
 struct TMyStruct {
     double foo;
@@ -16,8 +22,24 @@ struct TStudent {
     bool operator>(const TStudent &other) const { return grade > other.grade; }
 };
 
+////////////////////////////////////////////////////////////////////////////////////
+/// Step 2
+////////////////////////////////////////////////////////////////////////////////////
+
+static int Add(int a, int b) {
+    return a + b;
+}
+
+static std::string Greet(const std::string& name) {
+    return std::string("Hello ") + name;
+}
+
+using GreetPtr = std::string (*)(const std::string&);
+
 int main() {
 
+
+    // Step 1.1.a
     constexpr int a = 3;
     constexpr double b = 3.1;
 
@@ -28,12 +50,45 @@ int main() {
         .bar = a,
     });
 
+    // Step 1.1.b
     std::cout << "Max(1, 5) = " << GetMax(1, 5) << std::endl;
 
+    // Step 1.1.c
     constexpr TStudent s1 = {.grade = 3};
     constexpr TStudent s2 = {.grade = 5};
 
     std::cout << "Max(s1, s2) = " << GetMax(s1, s2).grade << std::endl;
+
+    // Step 1.2.a
+    const auto mytype = TDataPack<int, float, double, char>{};
+    
+    // Step 1.2.b
+    auto cls = TStaticArray<int, 5>{};
+    cls.Append(3);
+    cls.Append(6);
+    cls.Append(9);
+    cls.PrintItems();
+
+    auto stringcls = TStaticArray<const char*, 10>{};
+    stringcls.Append("Hello,");
+    stringcls.Append("World!");
+
+    // Step 1.2.c
+    auto wrapper = PrintContainer<TStaticArray, const char*, 10>;
+    wrapper(stringcls);
+
+    // Step 2.1.a
+    int (*add)(int, int) = &Add;
+    
+    // Explicit dereference
+    (*add)(2, 3);
+
+    // Implicit dereference
+    add(2, 3);
+
+    // Step 2.1.b
+    GreetPtr greet = &Greet;
+    std::cout << greet("Ole") << std::endl;
 
     return 0;
 }
--- Submissions/Submission-01/template.h
+++ Submissions/Submission-01/template.h
@@ -4,6 +4,12 @@
 #define _TEMPLATE_H
 
 #include <iostream>
+#include <optional>
+
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Step 1.1
+////////////////////////////////////////////////////////////////////////////////////
 
 template <typename T> void PrintTypeSize(T v) {
     std::cout << "Size: " << sizeof(T) << std::endl;
@@ -11,5 +17,49 @@ template <typename T> void PrintTypeSize(T v) {
 
 template <typename T> T GetMax(T a, T b) { return a > b ? a : b; }
 
+////////////////////////////////////////////////////////////////////////////////////
+/// Step 1.2
+////////////////////////////////////////////////////////////////////////////////////
+
+template <typename... Args>
+struct TDataPack;
+
+template <typename Head, typename... Tail>
+struct TDataPack<Head, Tail...> {
+    Head value;
+    TDataPack<Tail...> next;
+};
+
+template<> struct TDataPack<> {};
+
+template <typename T, unsigned int size>
+class TStaticArray {
+  public:
+    T items[size]{};
+
+  
+    auto Append(const T& item) -> std::optional<unsigned int> {
+        if (m_index < size) {
+            items[m_index] = item;
+            m_index++;
+            return m_index - 1;
+        }
+        return std::nullopt;
+    }
+
+    void PrintItems() const {
+        for (auto i = 0; i < m_index; i++)
+            std::cout << items[i] << ' ';
+        std::cout << std::endl;
+    }
+
+  private:
+    unsigned int m_index{};
+};
+
+template <template <typename, int> class Container, typename T, unsigned int N>
+auto PrintContainer(const Container<T, N>& container) {
+    container.PrintItems();
+}
 
 #endif //_TEMPLATE_H
